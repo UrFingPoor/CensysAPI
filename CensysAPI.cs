23a7a776f939d1d9d4f356c0ac2de8af41b76c4b
Joshua <73680704+UrFingPoor@users.noreply.github.com>
2022-11-09 20:42:34 -0500

    Update CensysAPI.cs

--- CensysAPI.cs
+++ CensysAPI.cs
@@ -24,16 +24,16 @@ namespace ExampleProject
                     case HttpStatusCode.OK:
                         response = JObject.Parse(await result.Content.ReadAsStringAsync());
                         File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + $"{host}.json", Convert.ToString(response));
-                        MessageBox.Show($"Saved To {AppDomain.CurrentDomain.BaseDirectory + $"{host}.json"}!", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show($"Saved To {AppDomain.CurrentDomain.BaseDirectory + $"{host}.json"}!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         break;
                     case HttpStatusCode.BadRequest:
-                        MessageBox.Show($"Bad Request: The request you made is invalid.", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show($"Bad Request: The request you made is invalid.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         break;
                     case HttpStatusCode.Unauthorized:
-                        MessageBox.Show("Unauthorized: You must authenticate with a valid API ID and secret.", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("Unauthorized: You must authenticate with a valid API ID and secret.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         break;
                     case (HttpStatusCode)422:
-                        MessageBox.Show("invalid cursor: Remove The https://.", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("invalid cursor: Remove The https://.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         break;
                     case (HttpStatusCode)429:
                         MessageBox.Show("Too many requests: Please wait abit before resending the request.", "The Bro's", MessageBoxButtons.OK, MessageBoxIcon.Error);
